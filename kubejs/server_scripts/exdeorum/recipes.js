4a1d1b5b69b38235081906c89f6809a42897a654
Sponeru <o0sponge0o_minecraft_flat@yahoo.co.jp>
2025-12-28 04:10:46 +0900

    More Machine導入に伴うアプグレレシピの削除とか

--- kubejs/server_scripts/exdeorum/recipes.js
+++ kubejs/server_scripts/exdeorum/recipes.js
@@ -1,7 +1,7 @@
 ServerEvents.recipes(event => {
     let removeItem = ['exdeorum:mechanical_hammer', 'exdeorum:mechanical_sieve', 'exdeorum:end_cake']
 
-    let ores = ['minecraft:coal', 'minecraft:raw_iron', 'minecraft:raw_copper', 'minecraft:raw_gold', 'mekanism:raw_osmium', 'mekanism:raw_tin', 'mekanism:raw_lead', 'mekanism:raw_uranium', 'minecraft:lapis_lazuli', 'minecraft:diamond', 'minecraft:emerald', 'kubejs:iridium', 'mekanism:fluorite_gem', 'minecraft:quartz', 'deepdepthnetherite:deepslate_netherite_ore', 'minecraft:redstone']
+    let ores = ['minecraft:coal', 'minecraft:raw_iron', 'minecraft:raw_copper', 'minecraft:raw_gold', 'mekanism:raw_osmium', 'mekanism:raw_tin', 'mekanism:raw_lead', 'mekanism:raw_uranium', 'minecraft:lapis_lazuli', 'minecraft:diamond', 'minecraft:emerald', 'kubejs:iridium', 'mekanism:fluorite_gem', 'minecraft:quartz', 'kubejs:ore_debri_deepslate', 'minecraft:redstone']
     let dense = ['denseores:dense_redstone_ore', 'denseores:dense_emerald_ore', 'denseores:dense_lapis_ore', 'denseores:dense_diamond_ore', 'kubejs:dense_ore_iridium', 'kubejs:dense_ore_quartz', 'densemekanism:dense_tin_ore', 'densemekanism:dense_osmium_ore', 'densemekanism:dense_uranium_ore', 'densemekanism:dense_fluorite_ore', 'densemekanism:dense_lead_ore', 'denseores:dense_coal_ore', 'denseores:dense_iron_ore', 'denseores:dense_copper_ore', 'denseores:dense_gold_ore']
     let allMesh = ['exdeorum:string_mesh', 'exdeorum:flint_mesh', 'exdeorum:iron_mesh', 'exdeorum:golden_mesh', 'exdeorum:diamond_mesh', 'exdeorum:netherite_mesh']
 
--- kubejs/server_scripts/mekanism/remove_machine_upgrade.js
+++ kubejs/server_scripts/mekanism/remove_machine_upgrade.js
@@ -9,4 +9,50 @@ ServerEvents.recipes(event => {
             event.remove({ id: `mekanism_extras:factory/cosmic/${id}` }),
             event.remove({ id: `mekanism_extras:factory/infinite/${id}` })
     })
+
+    var mmlist = ['planting', 'recycling', 'stamping', 'lathing', 'rolling_mill', 'replicating']
+
+    mmlist.forEach((id)=>{
+            event.remove({ id: `mekmm:factory/basic/${id}` }),
+            event.remove({ id: `mekmm:factory/advanced/${id}` }),
+            event.remove({ id: `mekmm:factory/elite/${id}` }),
+            event.remove({ id: `mekmm:factory/ultimate/${id}` })
+    })
+
+    var advMachineList = ["oxidizing","chemical_infusing","washing","dissolving","crystallizing","pressurised_reacting","centrifuging","liquifying"]
+    
+    let machineUpgradeMap = new Map([
+        ["recycling",'mekmm:recycler'],
+        ["planting",'mekmm:planting_station'],
+        ["stamping",'mekmm:cnc_stamper'],
+        ["oxidizing",'mekanism:chemical_oxidizer'],
+        ["chemical_infusing",'mekanism:chemical_infuser'],
+        ["washing",'mekanism:chemical_washer'],
+        ["dissolving",'mekanism:chemical_dissolution_chamber'],
+        ["crystallizing",'mekanism:chemical_crystallizer'],
+        ["pressurised_reacting",'mekanism:pressurized_reaction_chamber'],
+        ["centrifuging",'mekanism:isotopic_centrifuge'],
+        ["liquifying",'mekanism:nutritional_liquifier']        
+        ])
+
+    //lathing
+    //replicating
+    //rolling_mill
+
+    advMachineList.forEach((id)=>{
+            event.remove({ id: `mekmm:factory/basic/${id}` }),
+            event.remove({ id: `mekmm:factory/advanced/${id}` }),
+            event.remove({ id: `mekmm:factory/elite/${id}` }),
+            event.remove({ id: `mekmm:factory/ultimate/${id}` })
+    })
+        
+    advMachineList.concat(mmlist).forEach((id)=>{
+        ["basic","advanced","elite","ultimate"].forEach((tier,index,array)=>{
+                if(tier != "basic") event.recipes.minecraft.crafting_shapeless(`mekmm:${tier}_${id}_factory`,[`mekmm:${array[index-1]}_${id}_factory`,`mekanism:${tier}_tier_installer`])
+        })
+    })
+
+    machineUpgradeMap.forEach((value,key)=>{
+        event.recipes.minecraft.crafting_shapeless(`mekmm:basic_${key}_factory`,[value,`mekanism:basic_tier_installer`])
+    })
 })
