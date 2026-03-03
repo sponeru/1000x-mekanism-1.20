ff4b712928619fd215b2e8df3bd6498dbfa74263
Sponeru <o0sponge0o_minecraft_flat@yahoo.co.jp>
2026-03-03 16:46:20 +0900

    液体水素のタグミスを修正

--- kubejs/server_scripts/mekanism/normal/ore_process/tag.js
+++ kubejs/server_scripts/mekanism/normal/ore_process/tag.js
@@ -2,3 +2,7 @@ ServerEvents.tags("item",(event) => {
     event.add('forge:ores/quartz', 'kubejs:ore_quartz')
     event.add('forge:ores/quartz', 'kubejs:ore_quartz_deepslate')
 })
+
+ServerEvents.tags("fluid",(event)=>{
+    event.add('forge:hydrogen', 'mekanism:hydrogen')
+})
