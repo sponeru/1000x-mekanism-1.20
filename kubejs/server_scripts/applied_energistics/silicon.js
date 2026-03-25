49a62a83113487fd0e89f6c96d2fc160092372aa
Sponeru <o0sponge0o_minecraft_flat@yahoo.co.jp>
2026-03-25 21:40:26 +0900

    クオーツ混合物の追加

---
+++ kubejs/server_scripts/applied_energistics/silicon.js
@@ -0,0 +1,5 @@
+ServerEvents.recipes(event => { 
+    event.shapeless('6x mek1000:quartz_compose',['ae2:certus_quartz_dust','2x #minecraft:coals','6x #minecraft:sand'])
+
+    event.smelting('4x ae2:silicon','mek1000:quartz_compose')
+})
--- kubejs/startup_scripts/other_items.js
+++ kubejs/startup_scripts/other_items.js
@@ -13,7 +13,8 @@ StartupEvents.registry("item", event => {
     event.create('mek1000:dust_amethyst').texture('kubejs:item/dust_iridium').color(0, "#9400d3").tag('forge:dusts').tag('forge:dusts/amethyst')
     
     event.create('mek1000:bio_compose').texture('kubejs:item/dust_iridium').color(0, "#00581b")
-    
+    event.create('mek1000:quartz_compose').texture('kubejs:item/dust_iridium').color(0, "#e8fc7b")
+
     event.create('mek1000:experience_crystal').texture('kubejs:item/crystal_fluorite').color(0, "#1ffa43")
     event.create('mek1000:bedrockium_ingot')
     event.create("mek1000:ultimate_alloy_ingot")
