c1aafddc9897bd1997959b84f4055e234a7f0682
Sponeru <o0sponge0o_minecraft_flat@yahoo.co.jp>
2026-04-30 18:59:01 +0900

    レシピの変更とか

--- kubejs/server_scripts/circulation_flow_networks/recipes.js
+++ kubejs/server_scripts/circulation_flow_networks/recipes.js
@@ -1,11 +1,11 @@
 ServerEvents.recipes(event => {
     event.remove({output:'circulation_networks:sourceflow_crystal'})
-    event.recipes.mekanism.metallurgic_infusing('circulation_networks:sourceflow_crystal','mek1000:crystal_certus_quartz', "100x astral_mekanism:singularity")
+    event.recipes.mekanism.metallurgic_infusing('circulation_networks:sourceflow_crystal','fluxnetworks:flux_core', "100x mekanism:diamond")
 
     event.remove({output:'circulation_networks:netherforged_crystal'})
-    event.recipes.mekanism.metallurgic_infusing('circulation_networks:netherforged_crystal','mek1000:crystal_redstone', "100x astral_mekanism:singularity")
+    event.recipes.mekanism.metallurgic_infusing('circulation_networks:netherforged_crystal','fluxnetworks:flux_core', "100x mekanism:redstone")
 
     event.remove({output:'circulation_networks:endercore_crystal'})
-    event.recipes.mekanism.metallurgic_infusing('circulation_networks:endercore_crystal','mek1000:crystal_quartz', "100x astral_mekanism:singularity")
+    event.recipes.mekanism.metallurgic_infusing('circulation_networks:endercore_crystal','fluxnetworks:flux_core', "100x mekanism:gold")
 
 })
